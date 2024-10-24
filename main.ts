2fbb1d7d4fd4076b7fe489683e10635aa74bf2ba
Jan Strnad <jan.strnad13@gmail.com>
2024-10-24 19:20:05 +0200

    arrow fnc

--- main.ts
+++ main.ts
@@ -3,14 +3,14 @@ import * as fs from 'fs';
 import * as path from 'path';
 import { StockEvent } from './src/Event';
 
-function toPascalCase(str: string): string {
+const toPascalCase = (str: string): string => {
     return str
         .replace(/[^a-zA-Z0-9]+(.)/g, (_, chr) => chr.toUpperCase())
         .replace(/[^a-zA-Z0-9]/g, '')
         .replace(/^./, chr => chr.toUpperCase());
 }
 
-async function readCSV(filePath: string): Promise<StockEvent[]> {
+const readCSV = async (filePath: string): Promise<StockEvent[]> => {
     const records: any[] = [];
     const parser = fs
         .createReadStream(filePath)
@@ -26,18 +26,12 @@ async function readCSV(filePath: string): Promise<StockEvent[]> {
     return records;
 }
 
-async function main() {
+const main = async () => {
     try {
         const csvPath = path.join(__dirname, './data/from_2019-12-04_to_2020-12-03_MTcyOTY2NzkzMDk4Ng.csv');
         const csvData = await readCSV(csvPath);
 
-        console.log('CSV Data:', csvData);
 
-        // Example: Access specific data
-        if (csvData.length > 0) {
-            console.log('First row:', csvData[0]);
-            console.log('Second row:', csvData[1]);
-        }
     } catch (error) {
         console.error('Error reading CSV:', error);
     }

509df6791e4edb1eb7a6b8d995f8d39464e08e70
Jan Strnad <jan.strnad13@gmail.com>
2024-10-27 17:51:07 +0100

    full run

--- main.ts
+++ main.ts
@@ -11,8 +11,19 @@ import { enhanceCSVWithSplits } from './src/modules/enhnace/enhace';
 
 const main = async () => {
     try {
-        const csvPath = path.join(__dirname, './data/from_2019-12-04_to_2020-12-03_MTcyOTY2NzkzMDk4Ng.csv');
-        const csvData = await readCSV(csvPath);
+        const csvPath1 = path.join(__dirname, './data/from_2019-12-04_to_2020-12-03_MTcyOTY2NzkzMDk4Ng.csv');
+        const csvData1 = await readCSV(csvPath1);
+
+        const csvPath2 = path.join(__dirname, './data/from_2021-12-04_to_2022-12-03_MTcyOTY2ODA0OTkyOQ.csv');
+        const csvData2 = await readCSV(csvPath2);
+
+        const csvPath3 = path.join(__dirname, './data/from_2022-12-04_to_2023-12-03_MTcyOTY2ODA3OTAyMg.csv');
+        const csvData3 = await readCSV(csvPath3);
+
+        const csvPath4 = path.join(__dirname, './data/from_2023-12-04_to_2024-10-23_MTcyOTY2ODEyNTM2Mw.csv');
+        const csvData4 = await readCSV(csvPath4);
+
+        const csvData = [...csvData1, ...csvData2, ...csvData3, ...csvData4];
 
         //  list of all the unique stock tickets   
         const allTickers = csvData
